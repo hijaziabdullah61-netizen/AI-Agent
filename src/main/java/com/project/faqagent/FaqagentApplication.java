1e4581b96a3297ed94435d58ea7071cd00c5d576
Abdullah Hijazi <hijaziabdullah61@gmail.com>
2026-07-05 15:06:56 +0300

    Auto-parse DATABASE_URL for Railway

--- src/main/java/com/project/faqagent/FaqagentApplication.java
+++ src/main/java/com/project/faqagent/FaqagentApplication.java
@@ -3,10 +3,31 @@ package com.project.faqagent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URI;
+
 @SpringBootApplication
 public class FaqagentApplication {
 
 	public static void main(String[] args) {
+		String dbUrl = System.getenv("DATABASE_URL");
+		if (dbUrl != null && dbUrl.startsWith("postgres")) {
+			try {
+				URI uri = new URI(dbUrl);
+				if (uri.getUserInfo() != null) {
+					String[] userInfo = uri.getUserInfo().split(":");
+					System.setProperty("spring.datasource.username", userInfo[0]);
+					if (userInfo.length > 1) {
+						System.setProperty("spring.datasource.password", userInfo[1]);
+					}
+				}
+				String jdbcUrl = "jdbc:postgresql://" + uri.getHost() + 
+						(uri.getPort() != -1 ? ":" + uri.getPort() : "") + 
+						uri.getPath();
+				System.setProperty("spring.datasource.url", jdbcUrl);
+			} catch (Exception e) {
+				System.err.println("Failed to parse DATABASE_URL: " + e.getMessage());
+			}
+		}
 		SpringApplication.run(FaqagentApplication.class, args);
 	}
 
